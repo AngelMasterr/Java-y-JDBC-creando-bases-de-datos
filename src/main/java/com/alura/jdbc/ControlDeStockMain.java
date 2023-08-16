a20c4e5cb46a3449d0b792de2be47ff71af395b5
AngelMasterr <angelduarteprog@gmail.com>
2023-08-15 23:13:09 -0500

    empezando a crear ventanas

--- src/main/java/com/alura/jdbc/ControlDeStockMain.java
+++ src/main/java/com/alura/jdbc/ControlDeStockMain.java
@@ -9,6 +9,7 @@ public class ControlDeStockMain {
 	public static void main(String[] args) {
 		ControlDeStockFrame produtoCategoriaFrame = new ControlDeStockFrame();
 		produtoCategoriaFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		System.out.println("funciona");
 	}
 
 }
