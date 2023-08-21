58628e772ba501dc660a1ccf21cce9c20d0978d7
AngelMasterr <angelduarteprog@gmail.com>
2023-08-20 21:33:48 -0500

    usando try para cerrar las conexiones abiertas

--- src/main/java/com/alura/jdbc/controller/ProductoController.java
+++ src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -2,6 +2,7 @@ package com.alura.jdbc.controller;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -18,76 +19,115 @@ import com.mysql.cj.xdevapi.Statement;
 public class ProductoController {
 
 	public int modificar(String nombre, String descripcion, Integer id, Integer cantidad) throws SQLException {
-		
-		Connection con = new ConnectionFactory().recuperaConexion();
-		java.sql.Statement statement = con.createStatement();
-		
-		statement.execute("UPDATE PRODUCTOS SET "
-			    + " NOMBRE = '" + nombre + "'"
-			    + ", DESCRIPCION = '" + descripcion + "'"
-			    + ", CANTIDAD = " + cantidad
-			    + " WHERE ID = " + id);
-				 		
-		System.out.println(String.format("%s, %s, %d, %d", nombre, descripcion, cantidad, id ));
-		return statement.getUpdateCount();
+
+		final Connection con = new ConnectionFactory().recuperaConexion();
+		try (con) {
+
+			final PreparedStatement statement = con.prepareStatement(
+					"UPDATE PRODUCTOS SET " + " NOMBRE = ?," + " DESCRIPCION = ?," + " CANTIDAD = ?" + " WHERE ID = ?");
+			try (statement) {
+				
+				statement.setString(1, nombre);
+				statement.setString(2, descripcion);
+				statement.setInt(3, cantidad);
+				statement.setInt(4, id);
+
+				statement.execute();
+
+				System.out.println(String.format("%s, %s, %d, %d", nombre, descripcion, cantidad, id));
+				return statement.getUpdateCount();
+			}
+		}
 	}
 
 	public int eliminar(Integer id) throws SQLException {
-		
-		Connection con = new ConnectionFactory().recuperaConexion();
-		java.sql.Statement statement = con.createStatement();
-		
-		statement.execute("DELETE FROM PRODUCTOS WHERE ID = " + id);
-				
-		return statement.getUpdateCount();		 
-		
+
+		final Connection con = new ConnectionFactory().recuperaConexion();
+		try (con) {
+
+			final PreparedStatement statement = con.prepareStatement("DELETE FROM PRODUCTOS WHERE ID = ?");
+			try (statement) {
+
+				statement.setInt(1, id);
+				statement.execute();
+				return statement.getUpdateCount();
+			}
+		}
 	}
 
 	public List<Map<String, String>> listar() throws SQLException {
-		
-		Connection con = new ConnectionFactory().recuperaConexion();
-		
-		java.sql.Statement statement = con.createStatement();
-		
-		statement.execute("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM PRODUCTOS");
-			
-		ResultSet resultSet = statement.getResultSet();
-		
-		List<Map<String, String>> resultado = new ArrayList<>();
-		
-		while (resultSet.next()) {
-			Map<String, String> fila = new HashMap<>();
-			fila.put("id", String.valueOf(resultSet.getInt("id")));
-			fila.put("nombre", resultSet.getString("nombre"));
-			fila.put("descripcion", resultSet.getString("descripcion"));
-			fila.put("cantidad", String.valueOf(resultSet.getInt("cantidad")));
-			
-			resultado.add(fila);
+
+		final Connection con = new ConnectionFactory().recuperaConexion();
+		try (con) {
+
+			final PreparedStatement statement = con
+					.prepareStatement("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM PRODUCTOS");
+			try (statement) {
+				statement.execute();
+
+				ResultSet resultSet = statement.getResultSet();
+
+				List<Map<String, String>> resultado = new ArrayList<>();
+
+				while (resultSet.next()) {
+					Map<String, String> fila = new HashMap<>();
+					fila.put("id", String.valueOf(resultSet.getInt("id")));
+					fila.put("nombre", resultSet.getString("nombre"));
+					fila.put("descripcion", resultSet.getString("descripcion"));
+					fila.put("cantidad", String.valueOf(resultSet.getInt("cantidad")));
+
+					resultado.add(fila);
+				}
+				return resultado;
+			}
 		}
-			
-		con.close();
-		
-		return resultado;
 	}
 
-    public void guardar(Map<String, String> producto) throws SQLException {
-		Connection con = new ConnectionFactory().recuperaConexion();
-		
-		java.sql.Statement statement = con.createStatement();
-		
-		statement.execute("INSERT INTO PRODUCTOS(nombre, descripcion, cantidad)"
-				+ "VALUES('" +producto.get("nombre") + "','"
-				+ producto.get("descripcion") + "',"
-				+ producto.get("cantidad") + ")", java.sql.Statement.RETURN_GENERATED_KEYS );
-		
-		ResultSet resultSet = statement.getGeneratedKeys();		
-				
-		while (resultSet.next()) {
-			System.out.println(String.format(
-					"fue insertado el producto del id %d", resultSet.getInt(1)));			
+	public void guardar(Map<String, String> producto) throws SQLException {
+		String nombre = producto.get("nombre");
+		String descripcion = producto.get("descripcion");
+		Integer cantidad = Integer.valueOf(producto.get("cantidad"));
+		Integer maxCantidad = 50;
+
+		final Connection con = new ConnectionFactory().recuperaConexion();
+		try (con) {
+			con.setAutoCommit(false);
+
+			final PreparedStatement statement = con.prepareStatement(
+					"INSERT INTO PRODUCTOS " + "(nombre, descripcion, cantidad)" + "VALUES(?, ?, ?)",
+					java.sql.Statement.RETURN_GENERATED_KEYS);
+
+			try (statement) {
+				do {
+					int cantidadParaGuardar = Math.min(cantidad, maxCantidad);
+					ejecutaRegistro(nombre, descripcion, cantidadParaGuardar, statement);
+					cantidad -= maxCantidad;
+					System.out.println(cantidad);
+
+				} while (cantidad > 0);
+
+				con.commit();
+			} catch (Exception e) {
+				con.rollback();
+			}
+
 		}
-		
-		
+	}
+
+	private void ejecutaRegistro(String nombre, String descripcion, Integer cantidad, PreparedStatement statement)
+			throws SQLException {
+		statement.setString(1, nombre);
+		statement.setString(2, descripcion);
+		statement.setInt(3, cantidad);
+
+		statement.execute();
+
+		final ResultSet resultSet = statement.getGeneratedKeys();
+		try (resultSet) {
+			while (resultSet.next()) {
+				System.out.println(String.format("fue insertado el producto del id %d", resultSet.getInt(1)));
+			}
+		}
 	}
 
 }
